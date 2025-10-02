46f22a132dfafbf7b50f983e910af45b0e2e2001
Avi <95044844+Avioti@users.noreply.github.com>
2025-10-02 10:43:24 -0400

    Corrected?

--- src/main/java/com/pluralsight/Dice.java
+++ src/main/java/com/pluralsight/Dice.java
@@ -3,41 +3,7 @@ package com.pluralsight;
 public class Dice {
     public static void main(String[] args) {
 
-    Dice dice = new Dice();
 
-    int roll1 = 0;
-    int roll2 = 0;
-    int count_2 = 0;
-    int count_4 = 0;
-    int count_6 = 0;
-    int count_7 = 0;
-
-    for (int i = 0; i <= 100; i++){
-        roll1 = dice.roll();
-        roll2 = dice.roll();
-        int sum = roll1 + roll2;
-        switch (sum){
-            case 2:
-                count_2 += 1;
-                break;
-            case 4:
-                count_4 += 1;
-                break;
-            case 6:
-                count_6 += 1;
-                break;
-            case 7:
-                count_7 += 1;
-        }
-        System.out.println("Roll "+ i + ": " +  roll1 + " - " + roll2 + " Sum: " + sum );
-        if(i == 100){
-            System.out.println(count_2);
-            System.out.println(count_4);
-            System.out.println(count_6);
-            System.out.println(count_7);
-        }
-
-    }
 
 
     }
